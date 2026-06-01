da0a172bdbc3d66800543f77243d893418c16b03
joonho4 <wns4090@gmail.com>
2026-06-01 16:11:34 +0900

    [Feature] Entity 생성 및 DB 연결

--- backend/src/main/java/com/project/backend/global/common/entity/BaseEntity.java
+++ backend/src/main/java/com/project/backend/global/common/entity/BaseEntity.java
@@ -11,8 +11,10 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
     @CreatedDate
     private LocalDateTime createdAt;
+
     @LastModifiedDate
     private LocalDateTime updatedAt;
 }
