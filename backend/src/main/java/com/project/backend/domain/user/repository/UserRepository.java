2c5edfda0861dd7daf4a7c63560d81a8f79b8b4c
joonho4 <wns4090@gmail.com>
2026-06-02 14:30:04 +0900

    [Feature] 필요한 DB 조회 문 추가

--- backend/src/main/java/com/project/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/project/backend/domain/user/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.project.backend.domain.user.repository;
 import com.project.backend.domain.user.Entity.PromptUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<PromptUser, Long> {
 
     boolean existsByEmail(String email);
 
     boolean existsByNickname(String nickname);
+
+    Optional<PromptUser> findByEmail(String email);
 }
